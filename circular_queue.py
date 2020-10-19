3622b5fbc46645ecb4467888bb3c7232489d34ca
Venkat Prasad Krishnamurthy <krishnamurthy.v@husky.neu.edu>
2020-10-18 20:24:58 -0400

    added circular queue

---
+++ circular_queue.py
@@ -0,0 +1,86 @@
+class MyCircularQueue:
+
+    def __init__(self, k: int):
+        """
+        Initialize your data structure here. Set the size of the queue to be k.
+        """
+        self.queue = [0 for i in range(k)]
+        self.head = 0
+        self.tail = -1
+        self.size = 0
+        
+
+    def enQueue(self, value: int) -> bool:
+        """
+        Insert an element into the circular queue. Return true if the operation is successful.
+        """
+        if self.isFull():
+            return False
+        self.incrementTail()
+        self.queue[self.tail] = value
+        return True
+
+    def deQueue(self) -> bool:
+        """
+        Delete an element from the circular queue. Return true if the operation is successful.
+        """
+        if self.isEmpty():
+            return False
+        self.incrementHead()
+        return True
+
+    def Front(self) -> int:
+        """
+        Get the front item from the queue.
+        """
+        if self.isEmpty():
+            return -1
+        return self.queue[self.head]
+
+    def Rear(self) -> int:
+        """
+        Get the last item from the queue.
+        """
+        if self.isEmpty():
+            return -1
+        return self.queue[self.tail]
+
+    def isEmpty(self) -> bool:
+        """
+        Checks whether the circular queue is empty or not.
+        """
+        return self.size == 0
+
+    def isFull(self) -> bool:
+        """
+        Checks whether the circular queue is full or not.
+        """
+        if self.size == len(self.queue):
+            return True
+        return False
+    
+    def incrementTail(self):
+        if self.tail == len(self.queue) - 1:
+            self.tail = 0
+        else:
+            self.tail += 1
+        self.size += 1
+            
+    def incrementHead(self):
+        if self.head == len(self.queue) - 1:
+            self.head = 0
+        else:
+            self.head += 1
+        
+        self.size -= 1
+        
+
+
+# Your MyCircularQueue object will be instantiated and called as such:
+# obj = MyCircularQueue(k)
+# param_1 = obj.enQueue(value)
+# param_2 = obj.deQueue()
+# param_3 = obj.Front()
+# param_4 = obj.Rear()
+# param_5 = obj.isEmpty()
+# param_6 = obj.isFull()
